758c5a95c7567ccf4d902c69c0da41c002c6f89c
Salvador Pamanes <s.pamanes@gmail.com>
2014-12-01 21:11:11 -0600

    mac link

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ unamestr=`uname -s`
 if [[ "$unamestr" == 'Linux' ]]; then
     clear;g++ -g -DTOTAL_STARS=1200 -std=c++11 nbody.cpp main.cpp -lOpenCL -lGL -lGLU -lglut -op&&./p
 elif [[ "$unamestr" == 'Darwin' ]]; then
-    clear;g++ -g -DTOTAL_STARS=1200 -std=c++11 nbody.cpp main.cpp -framework OpenCL -framework OpenGL -framework GLUT -lglut -op&&./p
+    clear;g++ -g -DTOTAL_STARS=1200 -std=c++11 nbody.cpp main.cpp -framework OpenCL -framework OpenGL -framework GLUT -op&&./p
 fi
 
 
